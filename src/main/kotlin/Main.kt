d4b02b1b9f1021ccf87de55f41d404f1ac61dfe5
Heba-Alii <hebaaliali18@gmail.com>
2023-07-28 17:53:42 +0300

    kotlin fundamental

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+    var like: Float = 5.2f
+    like++
+    println(like)
+    //------------------------------
+    var isFollowing: Boolean = true
+    var isFollowingText: String = isFollowing.toString()
+    println(isFollowingText)
+    //--------------------------------
+    var friendCount: String = "317"
+    var friendNum: Int = friendCount.toInt()
+    friendNum++
+    println(friendNum)
+    println(friendCount.toInt() + 1)
+    //---------------------------------
+    println(220 > 120)
+    println(100 + 220)
+    //-------------------------------
+    var length = 120
+    println(length.minus(5))
+    println(length.plus(5))
+    //---------------------------------
+    var userAge: Int = 19
+    var userInfo = "User Age =${userAge + 1}"
+    println(userInfo)
+    //--------------------------------
+    println("Enter Your Age Please!!")
+    var userAger: Int = readln()!!.toInt()
+    var userInfor: String = "User Age = ${userAger + 1}"
+    println(userInfor)
+    //---------------------------------------
+    //Null Safety
+    val accountId: String? = null
+    println(accountId?.length)
+    // ?. -> if varaiable in null print null if variable is value print this value
+    // !! -> i'm sure that this vaariable contain a value not null
+    // ? -> (after data type) this varible possible contain null or contain value
+}

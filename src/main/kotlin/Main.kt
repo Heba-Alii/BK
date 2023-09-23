c21ef7c2dbaf6d0aa6c48e67f655fac6a409d040
Heba-Alii <hebaaliali18@gmail.com>
2023-09-23 23:38:38 +0300

    Multi Dimension Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1061,6 +1061,20 @@ fun calculateBirth(input: Int) = 2021 - input
 //----------------------------------------------
 //Multi Dimension Example
 
+//After becoming famous, the CodeBots decided to move into a new building together. Each of the rooms has a different cost, and some of them are free, but there's a rumour that all the free rooms are haunted! Since the CodeBots are quite superstitious, they refuse to stay in any of the free rooms, or any of the rooms below any of the free rooms.
+//
+//Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, your task is to return the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that don't appear below a 0).
+//
+//Example
+//
+//For
+//
+//matrix = [[0, 1, 1, 2],
+//[0, 5, 0, 0],
+//[2, 0, 3, 3]]
+//the output should be
+//solution(matrix) = 9.
+
 //fun solution(matrix: MutableList<MutableList<Int>>): Int {
 //    val rows = matrix.size
 //    val cols = matrix[0].size

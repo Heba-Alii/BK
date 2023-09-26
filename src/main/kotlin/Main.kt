24d28618422e7a82b180b4df2e9054e08457efab
Heba-Alii <hebaaliali18@gmail.com>
2023-09-26 13:22:36 +0300

    Matrix Example

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -860,30 +860,6 @@ fun sequences(sequence: MutableList<Int>): Boolean {
     return true
 }
 
-//-------------------------------------
-//Matrix Example
-fun matrixSolution(matrix: MutableList<MutableList<Int>>): Int {
-    val rows = matrix.size
-    val cols = matrix[0].size
-    var totalSum = 0
-
-    for (col in 0 until cols) {
-        var hauntedEncountered = false
-        for (row in 0 until rows) {
-            if (hauntedEncountered) {
-                matrix[row][col] = 0
-            } else {
-                totalSum += matrix[row][col]
-            }
-            if (matrix[row][col] == 0) {
-                hauntedEncountered = true
-            }
-        }
-    }
-
-    return totalSum
-}
-
 //-----------------------------------
 //Extension Function
 fun String.printTriple() {
